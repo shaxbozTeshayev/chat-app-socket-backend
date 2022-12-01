2a81eeb077f9b62146f0994df5244a849f5b5248
Shakhboz Teshaev <shaxbozteshayev7333@gmail.com>
2022-12-01 22:43:17 +0500

    last commit

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use("/api/conversation", conversationsRoute);
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-app-task-6.netlify.app/",
   },
 });
 
